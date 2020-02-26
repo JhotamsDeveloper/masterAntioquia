3174015bc8d7b3d47e89d2b6d74a73ab7daaf012
JhotamsDeveloper <Jhotams.developer@gmail.com>
2020-02-25 23:36:59 -0500

    Actualizando Place

--- src/model/Place.cs
+++ src/model/Place.cs
@@ -7,12 +7,19 @@ namespace Model
     public class Place
     {
         public int PlaceId { get; set; }
+        public string Nit { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string admin { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public string CoverPage { get; set; }
+        public string Logo { get; set; }
+        public int Contract { get; set; }
+        public Boolean State { get; set; }
+
         public int CategoryId { get; set; }
         public Category Category { get; set; }
-        public string Name { get; set; }
-        public string Location { get; set; }
-        public string admin { get; set; }
-        public string CoverPage { get; set; }
         public List<Product> Products { get; set; }
     }
 }
