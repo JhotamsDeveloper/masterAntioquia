0daf65813ed8dd8a124209cada1809740d31fa3d
JhotaMSdeveloper <jhotams.developer@gmail.com>
2020-05-04 02:23:11 -0500

    version 005 configurando url amigables

---
+++ src/GestionAntioquia/Controllers/CompanyController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.DTOs;
+using Persisten.Database;
+using Service;
+
+namespace GestionAntioquia.Controllers
+{
+    public class CompanyController : Controller
+    {
+
+        private readonly ICompanyService _companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+        // GET: hotel
+
+        public async Task<IActionResult> Allies()
+        {
+            var _aliados = await _companyService.GetAll();
+
+            var _nuevo = DateTime.Now.AddMonths(1);
+
+            var _viewAliados = (from a in _aliados
+                                select new PlacesAlliesDto
+                                {
+                                    PlaceId = a.PlaceId,
+                                    Name = a.Name,
+                                    Contract = a.Contract,
+                                    CoverPage = a.CoverPage,
+                                    Description = a.Description.Substring(0, 20),
+                                    DataCreate = DateTime.Parse(a.CreationDate).AddMonths(1),
+                                    New = "nuevo"
+                                });
+
+            return View(_viewAliados);
+        }
+
+        // GET: hotel/Details/5
+        public async Task<ActionResult> Details(string nameHotel)
+        {
+
+            if (nameHotel == null)
+            {
+                return NotFound();
+            }
+
+            var _detalleHotel = await _companyService.Details(nameHotel);
+
+            if (_detalleHotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(_detalleHotel);
+        }
+
+        // GET: hotel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: hotel/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: hotel/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: hotel/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: hotel/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: hotel/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/Service/CompanyService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Model;
+using Model.DTOs;
+using Persisten.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface ICompanyService
+    {
+        Task<IEnumerable<Place>> GetAll();
+        Task<PlaceDto> Details(string nameHotel);
+    }
+
+    public class CompanyService : ICompanyService
+    {
+        //Variables
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CompanyService(ApplicationDbContext context,
+                              IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Place>> GetAll()
+        {
+            var _listAliados = _context.Places.Where(X => X.State == true);
+            return (await _listAliados.ToListAsync());
+        }
+
+        public async Task<PlaceDto> Details(string nameHotel)
+        {
+            var _place = _mapper.Map<PlaceDto>(
+                    await _context.Places
+                    .Include(c => c.Category)
+                    .Where(s => s.State == true)
+                    .FirstOrDefaultAsync(m => m.Name == nameHotel)
+
+                );
+
+            return _mapper.Map<PlaceDto>(_place);
+        }
+    }
+}
